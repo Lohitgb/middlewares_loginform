19b6c974dafa46fa9c1b6d063d8c34801209aa22
lohitgb <lohitybhandari442@gmail.com>
2023-05-29 12:41:31 +0530

    file added

---
+++ indexxx.js
@@ -0,0 +1,51 @@
+// Import required dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Create Express app
+const app = express();
+
+// Configure body-parser middleware to handle form data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// Custom middleware for handling student login
+app.use('/login', (req, res, next) => {
+  // Retrieve form data from the request body
+  const { username, password } = req.body;
+ 
+  console.log(req.body)
+  // Perform validation
+  if (!username || !password) {
+    return res.status(400).send('Username and password are required.');
+  }
+
+  // Perform authentication (dummy logic for demonstration purposes)
+  if (username === 'student' && password === 'password') {
+    // Store the authenticated student in the request object
+    req.student = {
+      username,
+      role: 'student'
+    };
+    // Continue to the next middleware
+    next();
+  } else {
+        return res.status(401).send('Invalid username or password.');
+  }
+});
+
+// Protected route that requires student login
+app.get('/dashboard', (req, res) => {
+  // Access the authenticated student from the request object
+  const student = req.student;
+  res.send(`Welcome, ${student.username}! This is your student dashboard.`);
+});
+
+app.post('/login1',(req,res) => {
+  console.log('login success');
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
